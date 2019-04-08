03c36bbd2e2265fc3f20aca16e4a8c0e6498262f
yuriquezada <yuri_quezada@hotmail.com>
2019-04-08 18:17:02 -0500

    Muestra templates de ejemplo al dar click en desayuno y almuerzo con SPA

---
+++ main/templates.js
@@ -0,0 +1,14 @@
+export const templatesObject = {
+  desayuno: ` <p>Aquí tengo mi lista de desayunito</p>
+  `,
+  almuerzo: `<p>Aquí tengo mi lista de almuerzo</p>
+  `,
+  home: `
+          <p>Bienvenidoooos</p>
+          `,
+  different: `<div id="message">
+                <h2>404</h2>
+                <h1>Página no encontrada</h1>
+                <p>El archivo especificado no se encontró en este sitio web. Por favor, compruebe la URL para errores y vuelva a intentarlo.</p>
+              </div>`
+} 
