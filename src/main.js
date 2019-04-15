9f94bf6c2ee2dce5537e984717e984d64c0783cb
yuriquezada <yuri_quezada@hotmail.com>
2019-04-14 20:56:00 -0500

    Muestra función de firebase en main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,38 @@
 import { initializeBurgerRouter } from './route.js';
 
+const initialize = () => {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCtgymKSWV62YN8zimGsMlfDc0mruTi2qM",
+        authDomain: "burguer-queen-234c8.firebaseapp.com",
+        databaseURL: "https://burguer-queen-234c8.firebaseio.com",
+        projectId: "burguer-queen-234c8",
+        storageBucket: "burguer-queen-234c8.appspot.com",
+        messagingSenderId: "41829269712"
+    };
+    firebase.initializeApp(config);
+    initializeBurgerRouter();
+}
 
 
-window.onload = initializeBurgerRouter()
+window.addEventListener('load', initialize)
+
+
+// /*-----------------------------------------------------------*/
+
+// import { initRouterPetbook } from './route.js';
+
+// const inicializa = () => {
+//     var config = {
+//         apiKey: "AIzaSyCaSZCOTrO0WcdvI7sCVPNE5p6eiGj7tB4",
+//         authDomain: "petbook-cbdd2.firebaseapp.com",
+//         databaseURL: "https://petbook-cbdd2.firebaseio.com",
+//         projectId: "petbook-cbdd2",
+//         storageBucket: "petbook-cbdd2.appspot.com",
+//         messagingSenderId: "158794772915"
+//     };
+//     firebase.initializeApp(config);
+//     initRouterPetbook();
+// }
+
+// window.addEventListener('load', inicializa)
