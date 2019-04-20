02bc6a133c53be1fdb8bb5a1a6325bdec75321e0
yuriquezada <yuri_quezada@hotmail.com>
2019-04-20 12:39:38 -0500

    Modulación a una sola función: showNav()

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-const appendChildren = (childrenArray, element) => {
+const appendChildren = (element, childrenArray) => {
   childrenArray.forEach(child => {
-    return element.appendChild(child);
+    element.appendChild(child);
   });
+  return element;
 }
 
 const createElement = (tagNameString, classNameString, content) => {
@@ -11,49 +12,32 @@ const createElement = (tagNameString, classNameString, content) => {
   return element;
 }
 
-/*--------------------DESAYUNO----------------------*/
-
-const showUlD = () => {
-  const ulElem = document.createElement('ul');
-  ulElem.classList.add('ul-Desayuno');
-
-  const liSandwich = createElement('li', 'li-sandwich', `Sandwich`);
-  const liBebidasD = createElement('li', 'li-bebidas-d', `Bebidas`);
-
-  const ul2 = [liSandwich, liBebidasD]
-
-  appendChildren(ul2, ulElem);
-
-  return ulElem;
+const showNav = (element, child) => {
+  element.addEventListener('click', () => {
+    const nav2 = document.getElementById("nav2");
+    nav2.innerHTML = '';
+    menu.innerHTML = '';
+    nav2.appendChild(child);
+  })
 }
 
+/*--------------------DESAYUNO----------------------*/
+
 const desayuno = document.getElementById('desayuno');
-desayuno.addEventListener('click', () => {
-  const nav2 = document.getElementById("nav2");
-  nav2.innerHTML = '';
-  menu.innerHTML = '';
-  nav2.appendChild(showUlD());
-})
+const ulElemD = createElement('ul', 'ul-desayuno', ' ');
+const liSandwich = createElement('li', 'li-sandwich', `Sandwich`);
+const liBebidasD = createElement('li', 'li-bebidas-d', `Bebidas`);
+const ul2D = [liSandwich, liBebidasD];
+const nodoD = appendChildren(ulElemD, ul2D)
+showNav(desayuno, nodoD)
 
 /*--------------------ALMUERZO----------------------*/
 
-const showUlA = () => {
-  const ulElem = document.createElement('ul');
-
-  const liHamburguesa = createElement('li', 'li-hamburguesa', `Hamburguesas`);
-  const liBebidasA = createElement('li', 'li-bebidas-a', `Bebidas`);
-
-  ulElem.classList.add('ul-Almuerzo');
-  ulElem.appendChild(liBebidasA);
-  ulElem.appendChild(liHamburguesa);
-
-  return ulElem;
-}
-
 const almuerzo = document.getElementById('almuerzo');
-almuerzo.addEventListener('click', () => {
-  const nav2 = document.getElementById("nav2");
-  nav2.innerHTML = '';
-  menu.innerHTML = '';
-  nav2.appendChild(showUlA());
-})
+const ulElemA = createElement('ul', 'ul-desayuno', ' ');
+const liHamburguesa = createElement('li', 'li-hamburguesa', `Hamburguesas`);
+const acompañamientos = createElement('li', 'acompañamientos', `Acompañamientos`);
+const liBebidasA = createElement('li', 'li-bebidas-a', `Bebidas`);
+const ul2A = [liHamburguesa, acompañamientos, liBebidasA];
+const nodoA = appendChildren(ulElemA, ul2A)
+showNav(almuerzo, nodoA)
